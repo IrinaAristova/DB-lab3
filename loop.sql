5268e817a8173d49c63f805bcafc163ff79ebd27
AristovaIrina <43518375+IrinaAristova@users.noreply.github.com>
2020-05-05 14:29:46 +0300

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -1,10 +1,13 @@
 DECLARE
-    start_year INT NOT NULL DEFAULT 2020;
+    start_year INT NOT NULL DEFAULT 2012;
     cnt INT NOT NULL DEFAULT 5;
 BEGIN
- 
+
+    DELETE FROM Cities WHERE Olimpiada_olimp_year >= start_year;
+    DELETE FROM Olimpiada WHERE olimp_year >= start_year;  
+    
     FOR i in 1..cnt LOOP
-        INSERT INTO Cities (Olimpiada.olimp_year) values (start_year + (i-1)*4);
-        INSERT INTO Cities (city_name) values ("city_name" || i, start_year + (i-1)*4);
+        INSERT INTO Olimpiada (olimp_year) values (start_year + (i-1)*4);
+        INSERT INTO Cities (city_name, Olimpiada_olimp_year) values ('city_name' || i, start_year + (i-1)*4);
     END LOOP;
 END;
